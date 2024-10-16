7335935180bc83f1a1dc9d458a3a6ee1dc027137
joaromasodun <joaromasodun@gmail.com>
2024-10-16 13:18:27 +0100

    login page created

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,45 @@
+import { FaEye } from "react-icons/fa";
+import Button from '../UI/Button'
 
 export default function Login() {
   return (
-    <div className="shadow-box-shadow-4 my-10 container mx-auto justify-center h-screen">
-      <div className="border max-w-[500px]">
-        <div className="input-group flex flex-col">
-          <label htmlFor="">Email</label>
-          <input type="email" placeholder="email" className=""/>
-        </div>
+    <section className="flex items-center h-screen">
+      <div className="container p-4">
+        <div className="flex justify-center">
+          <div className="shadow-box-shadow-4 max-w-[400px] w-full p-8">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-3xl">Login</h2>
+            <form>
+              <div className="input-group flex flex-col mb-6">
+                <label className="mb-2">Email</label>
+                <input 
+                  type="text" 
+                  placeholder="user@email.com" 
+                  className="focus:ring-0"/>
+              </div>
 
-        <div className="input-group flex flex-col">
-          <label htmlFor="">Password</label>
-          <input type="email" placeholder="email" className=""/>
+              <div className="input-group flex flex-col mb-6">
+                <label className="mb-2">Password</label>
+                <div className="flex border items-center">
+                  <input 
+                    type="password" 
+                    placeholder="**********" 
+                    className="border-0 focus:ring-0"/>
+                  <FaEye className="justify-end"/>
+                </div>
+              </div>
+
+              <span className="block mb-4">Forgot Password?</span>
+
+              <Button className="text-center bg-primary p-2 text-white mb-4 rounded-sm">Login</Button>
+
+              <div className="flex space-x-2">
+                <p>Dont have an account?</p>
+                <a href="#" className="text-primary"> Sign up</a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
